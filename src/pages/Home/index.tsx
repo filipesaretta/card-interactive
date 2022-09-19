40ce03919570f09667d6540f0fd295992567b9fb
filipesaretta <filipessm124@gmail.com>
2022-09-19 18:58:21 -0300

    Create data state for controlled form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import pattern from "../../assets/images/bg-main-desktop.png";
 import cardFront from "../../assets/images/bg-card-front.png";
 import cardBack from "../../assets/images/bg-card-back.png";
+import { useState } from "react";
 
 const HomeContainer = styled.div`
   display: grid;
@@ -29,12 +30,16 @@ const CardsContainer = styled.div`
 
 const card = styled.div`
   position: absolute;
-  width: 350px;
-  height: 200px;
+  width: 320px;
+  height: 170px;
   box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
   border-radius: 9px;
   left: 50%;
   transform: translateX(-50%);
+  @media (min-width: 400px) {
+    width: 350px;
+    height: 200px;
+  }
   @media (min-width: 1024px) {
     left: 0;
     transform: translateX(0);
@@ -42,9 +47,28 @@ const card = styled.div`
 `;
 
 const FrontCard = styled(card)`
+  display: grid;
   background-image: url(${cardFront});
   top: 10rem;
   z-index: 3;
+  padding: 1rem;
+  color: white;
+
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    span {
+      grid-column: 1 / -1;
+      font-weight: bold;
+      font-size: 1.5rem;
+    }
+
+    p:last-child {
+      justify-self: end;
+    }
+  }
+
   @media (min-width: 1024px) {
     top: 8rem;
     left: 2rem;
@@ -58,6 +82,16 @@ const BackCard = styled(card)`
   background-image: url(${cardBack});
   top: 1rem;
   left: 55%;
+  position: relative;
+  color: white;
+  font-weight: bold;
+
+  p {
+    position: absolute;
+    left: 88%;
+    top: 108px;
+  }
+
   @media (min-width: 1024px) {
     top: 22rem;
     left: 4rem;
@@ -84,39 +118,78 @@ const Details = styled.div`
   }
 `;
 
+interface dataProps {
+  cardHolder: string;
+  cardNumber: number;
+  validity: number;
+  cvc: number;
+}
+
 export function Home() {
+  const [cardData, setCardData] = useState<dataProps>({} as dataProps);
+
+  function handleChange(e) {
+    setCardData({ ...cardData, [e.target.name]: e.target.value });
+  }
+
   return (
     <HomeContainer>
       <CardsContainer>
-        {/* <img src={pattern} alt="" /> */}
-
         <FrontCard>
-          <p>Testing</p>
+          <span>LOGO</span>
+          <div>
+            <span>{cardData.cardNumber}</span>
+            <p>{cardData.cardHolder}</p>
+            <p>{cardData.validity}</p>
+          </div>
         </FrontCard>
         <BackCard>
-          <p>Testing</p>
+          <p>{cardData.cvc}</p>
         </BackCard>
       </CardsContainer>
       <Details>
         <form action="">
           <label htmlFor="CardHolder">
             Card Holder
-            <input type="text" id="CardHolder" />
+            <input
+              type="text"
+              id="CardHolder"
+              name="cardHolder"
+              value={cardData.cardHolder}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor="CardNumber">
             Card Number
-            <input type="text" id="CardNumber" />
+            <input
+              type="text"
+              id="CardNumber"
+              name="cardNumber"
+              value={cardData.cardNumber}
+              onChange={handleChange}
+            />
           </label>
 
           <div>
             <label htmlFor="ExpDate">
               Exp. Date (MM/YY)
-              <input type="text" />
+              <input
+                type="text"
+                name="validity"
+                value={cardData.validity}
+                onChange={handleChange}
+              />
             </label>
 
             <label htmlFor="cvc">
               CVC
-              <input type="text" id="cvc" />
+              <input
+                type="text"
+                id="cvc"
+                name="cvc"
+                value={cardData.cvc}
+                onChange={handleChange}
+              />
             </label>
           </div>
         </form>
